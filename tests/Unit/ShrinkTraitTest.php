6105fd1eeb8f8e08e2b339a0eec28c179d8321ec
David Wright <dave@coderstudios.com>
2019-07-24 00:51:39 +0100

    Update Shrink Trait Test

--- tests/Unit/ShrinkTraitTest.php
+++ tests/Unit/ShrinkTraitTest.php
@@ -18,5 +18,6 @@ class ShrinkTraitTest extends TestCase
         $t = new class { use Shrink; }; // this is an anonymous class great for testing traits
         $this->assertTrue($t->shrink('<body>Hello</body>') === '<body>Hello</body>');
         $this->assertTrue($t->shrink('<body> <p>Hello</p> </body>') === '<body><p>Hello</p></body>');
+        $this->assertTrue($t->shrink('<body>   <p>Hello</p>   </body>') === '<body><p>Hello</p></body>');
     }
 }
